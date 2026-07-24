13e087008e184d2536e4a7f52a1fc01dc731fcaa
lylst3r <lylnaho@gmail.com>
2026-07-23 18:07:26 -0600

    started page

---
+++ chickens.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chickens',
+  imports: [],
+  templateUrl: './chickens.html',
+  styleUrl: './chickens.scss',
+})
+export class ChickensPage {}
